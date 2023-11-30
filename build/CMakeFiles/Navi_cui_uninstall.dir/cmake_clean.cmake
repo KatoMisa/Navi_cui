a9aabbffd346128547b846087a3e5a5b3e247941
KatoMisa <112141989+KatoMisa@users.noreply.github.com>
2023-11-30 14:48:28 +0900

    Delete build directory

--- build/CMakeFiles/Navi_cui_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Navi_cui_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Navi_cui_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
